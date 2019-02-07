fd85ce8856dc68db32bdb5e1465178f058eb90ef
sagar <sagar@betterhalf.ai>
2019-02-07 22:02:55 +0530

    snake direction

--- snake.js
+++ snake.js
@@ -1,33 +1,71 @@
 
-class Snake {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.xSpeed = scale * 1;
-        this.ySpeed = 0;
-        this.draw = function () {
-            ctx.fillStyle = "#ffffff";
-            ctx.fillRect(this.x, this.y, scale, scale);
-        };
-        this.update = function () {
-            this.x += this.xSpeed;
-            this.y += this.ySpeed;
-        };
-        this.changeDirection = function (direction) {
-            switch (direction) {
-                case 'Up':
-                    this.xSpeed = 0;
-                    this.ySpeed = -scale * 1;
-                case 'Down':
-                    this.xSpeed = 0;
-                    this.ySpeed = scale * 1;
-                case 'Right':
-                    this.xSpeed = scale * 1;
-                    this.ySpeed = -scale * 1;
-                case 'Left':
-                    this.xSpeed = -scale * 1;
-                    this.ySpeed = 0;
-            }
-        };
+// class Snake {
+//     constructor() {
+//         this.x = 0;
+//         this.y = 0;
+//         this.xSpeed = scale * 1;
+//         this.ySpeed = 0;
+//         this.draw = function () {
+//             ctx.fillStyle = "#ffffff";
+//             ctx.fillRect(this.x, this.y, scale, scale);
+//         }
+//         this.update = function () {
+//             this.x += this.xSpeed;
+//             this.y += this.ySpeed;
+//         }
+//         this.changeDirection = function (direction) {
+//             switch (direction) {
+//                 case 'Up':
+//                     this.xSpeed = 0;
+//                     this.ySpeed = -scale * 1;
+//                 case 'Down':
+//                     this.xSpeed = 0;
+//                     this.ySpeed = scale * 1;
+//                 case 'Right':
+//                     this.xSpeed = scale * 1;
+//                     this.ySpeed = -scale * 1;
+//                 case 'Left':
+//                     this.xSpeed = -scale * 1;
+//                     this.ySpeed = 0;
+//             }
+//         }
+//     }
+// }
+
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = scale * 1;
+    this.ySpeed = 0;
+  
+    this.draw = function() {
+      ctx.fillStyle = "#FFFFFF";  
+      ctx.fillRect(this.x, this.y, scale, scale);
+    }
+  
+    this.update = function() {
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+    }
+  
+    this.changeDirection = function(direction) {
+      switch(direction) {
+        case 'Up':
+          this.xSpeed = 0;
+          this.ySpeed = -scale * 1;
+          break;
+        case 'Down':
+          this.xSpeed = 0;
+          this.ySpeed = scale * 1;
+          break;
+        case 'Left':
+          this.xSpeed = -scale * 1;
+          this.ySpeed = 0;
+          break;
+        case 'Right':
+          this.xSpeed = scale * 1;
+          this.ySpeed = 0;
+          break;
+      }
     }
 }
