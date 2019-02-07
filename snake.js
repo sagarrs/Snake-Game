2f7d305a8de198601fa9c3021fe8efd871576e98
sagar <sagar@betterhalf.ai>
2019-02-07 21:47:05 +0530

    minor change

--- snake.js
+++ snake.js
@@ -1,16 +1,33 @@
-function Snake(){
-    this.x = 0;
-    this.y = 0;
-    this.xSpeed = scale * 1;
-    this.ySpeed = 0;
 
-    this.draw = function(){
-        ctx.fillStyle = "#ffffff";
-        ctx.fillRect(this.x, this.y, scale, scale);
-    }
-
-    this.update = function(){
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
+class Snake {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.xSpeed = scale * 1;
+        this.ySpeed = 0;
+        this.draw = function () {
+            ctx.fillStyle = "#ffffff";
+            ctx.fillRect(this.x, this.y, scale, scale);
+        };
+        this.update = function () {
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+        };
+        this.changeDirection = function (direction) {
+            switch (direction) {
+                case 'Up':
+                    this.xSpeed = 0;
+                    this.ySpeed = -scale * 1;
+                case 'Down':
+                    this.xSpeed = 0;
+                    this.ySpeed = scale * 1;
+                case 'Right':
+                    this.xSpeed = scale * 1;
+                    this.ySpeed = -scale * 1;
+                case 'Left':
+                    this.xSpeed = -scale * 1;
+                    this.ySpeed = 0;
+            }
+        };
     }
 }
