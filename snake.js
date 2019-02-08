fe80189f74216eeac7b65355dc5c6f7648b9f73f
sagar <sagar@betterhalf.ai>
2019-02-08 19:45:24 +0530

    handling the out of boundry conditions

--- snake.js
+++ snake.js
@@ -1,37 +1,3 @@
-
-// class Snake {
-//     constructor() {
-//         this.x = 0;
-//         this.y = 0;
-//         this.xSpeed = scale * 1;
-//         this.ySpeed = 0;
-//         this.draw = function () {
-//             ctx.fillStyle = "#ffffff";
-//             ctx.fillRect(this.x, this.y, scale, scale);
-//         }
-//         this.update = function () {
-//             this.x += this.xSpeed;
-//             this.y += this.ySpeed;
-//         }
-//         this.changeDirection = function (direction) {
-//             switch (direction) {
-//                 case 'Up':
-//                     this.xSpeed = 0;
-//                     this.ySpeed = -scale * 1;
-//                 case 'Down':
-//                     this.xSpeed = 0;
-//                     this.ySpeed = scale * 1;
-//                 case 'Right':
-//                     this.xSpeed = scale * 1;
-//                     this.ySpeed = -scale * 1;
-//                 case 'Left':
-//                     this.xSpeed = -scale * 1;
-//                     this.ySpeed = 0;
-//             }
-//         }
-//     }
-// }
-
 function Snake() {
     this.x = 0;
     this.y = 0;
@@ -46,6 +12,19 @@ function Snake() {
     this.update = function() {
       this.x += this.xSpeed;
       this.y += this.ySpeed;
+
+      if(this.x > canvas.width){
+        this.x = 0;
+      }
+      if(this.y > canvas.height){
+        this.y = 0;
+      }
+      if(this.x < 0){
+        this.x = canvas.width;
+      }
+      if(this.y < 0){
+        this.y = canvas.height;
+      }
     }
   
     this.changeDirection = function(direction) {
